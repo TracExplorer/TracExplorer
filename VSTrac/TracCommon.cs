a10d5f1b084e337191b80374a0255e9f95796d02
Mladen Mihajlovic <mika76@users.noreply.github.com>
2008-04-28 20:46:59 +0000

    * Fixes #29 Server error: Server Committed a Protocol Violation Seciton=ResponseHeader detail=CR must be followed by LF  * Some unit testing changes

--- VSTrac/TracCommon.cs
+++ VSTrac/TracCommon.cs
@@ -26,6 +26,7 @@ using CookComputing.XmlRpc;
 using System.Net;
 using System.Security.Cryptography.X509Certificates;
 using System.Windows.Forms;
+using System.Reflection;
 
 namespace VSTrac
 {
@@ -38,6 +39,51 @@ namespace VSTrac
             {
                 return true;
             };
+
+            SetAllowUnsafeHeaderParsing();
+        }
+
+        /// <summary>
+        /// From http://xml-rpc.net/faq/xmlrpcnetfaq.html#2.27
+        /// See bug #29
+        /// </summary>
+        /// <returns></returns>
+        public static bool SetAllowUnsafeHeaderParsing()
+        {
+            //Get the assembly that contains the internal class
+            Assembly aNetAssembly = Assembly.GetAssembly(
+              typeof(System.Net.Configuration.SettingsSection));
+            if (aNetAssembly != null)
+            {
+                //Use the assembly in order to get the internal type for 
+                // the internal class
+                Type aSettingsType = aNetAssembly.GetType(
+                  "System.Net.Configuration.SettingsSectionInternal");
+                if (aSettingsType != null)
+                {
+                    //Use the internal static property to get an instance 
+                    // of the internal settings class. If the static instance 
+                    // isn't created allready the property will create it for us.
+                    object anInstance = aSettingsType.InvokeMember("Section",
+                      BindingFlags.Static | BindingFlags.GetProperty
+                      | BindingFlags.NonPublic, null, null, new object[] { });
+                    if (anInstance != null)
+                    {
+                        //Locate the private bool field that tells the 
+                        // framework is unsafe header parsing should be 
+                        // allowed or not
+                        FieldInfo aUseUnsafeHeaderParsing = aSettingsType.GetField(
+                          "useUnsafeHeaderParsing",
+                          BindingFlags.NonPublic | BindingFlags.Instance);
+                        if (aUseUnsafeHeaderParsing != null)
+                        {
+                            aUseUnsafeHeaderParsing.SetValue(anInstance, true);
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
         }
 
         /// <summary>
--- VSTracTests/XmlRpcTests.cs
+++ VSTracTests/XmlRpcTests.cs
@@ -31,12 +31,50 @@ using System.IO;
 using System.Diagnostics;
 using System.Drawing;
 using System.Security.Cryptography.X509Certificates;
+using System.Reflection;
 
 namespace VSTracTests
 {
     [TestClass]
     public class XmlRpcTests
     {
+        public static bool SetAllowUnsafeHeaderParsing()
+        {
+            //Get the assembly that contains the internal class
+            Assembly aNetAssembly = Assembly.GetAssembly(
+              typeof(System.Net.Configuration.SettingsSection));
+            if (aNetAssembly != null)
+            {
+                //Use the assembly in order to get the internal type for 
+                // the internal class
+                Type aSettingsType = aNetAssembly.GetType(
+                  "System.Net.Configuration.SettingsSectionInternal");
+                if (aSettingsType != null)
+                {
+                    //Use the internal static property to get an instance 
+                    // of the internal settings class. If the static instance 
+                    // isn't created allready the property will create it for us.
+                    object anInstance = aSettingsType.InvokeMember("Section",
+                      BindingFlags.Static | BindingFlags.GetProperty
+                      | BindingFlags.NonPublic, null, null, new object[] { });
+                    if (anInstance != null)
+                    {
+                        //Locate the private bool field that tells the 
+                        // framework is unsafe header parsing should be 
+                        // allowed or not
+                        FieldInfo aUseUnsafeHeaderParsing = aSettingsType.GetField(
+                          "useUnsafeHeaderParsing",
+                          BindingFlags.NonPublic | BindingFlags.Instance);
+                        if (aUseUnsafeHeaderParsing != null)
+                        {
+                            aUseUnsafeHeaderParsing.SetValue(anInstance, true);
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
         #region Skeleton Code
         private static ITrac trac = null;
 
@@ -49,9 +87,11 @@ namespace VSTracTests
                 return true;
             };
 
-            List<ServerDetails> servers = ServerDetails.LoadAll();
+            //List<ServerDetails> servers = ServerDetails.LoadAll();
 
-            trac = TracCommon.GetTrac(servers[0]); // get the first server from the registry
+            SetAllowUnsafeHeaderParsing();
+            //trac = TracCommon.GetTrac(servers[1]); // get the first server from the registry
+            trac = TracCommon.GetTrac(new ServerDetails("http://vstrac.devjavu.com/", "vstrac.devjavu@gmail.com", "testing"));
 
             xmlrpc.Tracer tracer = new xmlrpc.Tracer();
             tracer.Attach(trac);
