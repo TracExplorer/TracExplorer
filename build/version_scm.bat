db23043454aecc25afafd467c1bd7d1f43a3f149
linne@gmx.li <linne@gmx.li>
2008-09-02 18:34:31 +0000

    Fixed #47: Added automatic versioning. Now there are only two files (GlobalAssemblyInfo.cs.in & VersionNumberInclude.wxi.in), where the release manger has to set the actual version number and status comment. Added: some comments about the release procedure.

---
+++ build/version_scm.bat
@@ -0,0 +1,20 @@
+@echo off
+rem Get the latest version from the source code management and replace it inside a file
+rem More Information inside of the TortoiseSVN OnlineHelp under "SubWCRev"
+rem Param %1: Directory to analyse
+rem Param %2: File INPUT
+rem Param %3: File OUTPUT
+
+rem Search for subwcrev.exe from TortoiseSVN
+if not exist %ProgramFiles%\TortoiseSVN\bin\SubWCRev.exe goto error_no_subwcrev
+
+%ProgramFiles%\TortoiseSVN\bin\SubWCRev.exe %1 %2 %3 -f
+
+goto end
+
+:error_no_subwcrev
+echo No TortoiseSVN-Client (SubWCRev.exe) detected!
+exit /b 1
+
+:end
+exit %errorlevel%
--- src/TracExplorer.CommonTest/Properties/AssemblyInfo.cs
+++ src/TracExplorer.CommonTest/Properties/AssemblyInfo.cs
@@ -6,12 +6,6 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("VSTracTests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Absa")]
-[assembly: AssemblyProduct("VSTracTests")]
-[assembly: AssemblyCopyright("Copyright © Absa 2008")]
-[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -31,5 +25,3 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
